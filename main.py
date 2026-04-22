9313cefb3ac61d7348110b27a6809832d8d1b5b9
sehaj <singhsehajsukhleen@gmail.com>
2026-04-23 01:15:39 +0530

    the main file to interact with agent

---
+++ main.py
@@ -0,0 +1,37 @@
+from core.chatbot import bot
+from langchain_core.messages import HumanMessage
+
+def show_banner():
+    banner = r"""
+    ██╗     ███████╗ █████╗ ██████╗ ██╗  ██╗
+    ██║     ██╔════╝██╔══██╗██╔══██╗╚██╗██╔╝
+    ██║     █████╗  ███████║██║  ██║ ╚███╔╝ 
+    ██║     ██╔══╝  ██╔══██║██║  ██║ ██╔██╗ 
+    ███████╗███████╗██║  ██║██████╔╝██╔╝ ██╗
+    ╚══════╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝
+
+    LeadX AI — Social-to-Lead Agent
+    -----------------------------------
+
+    - Persistent Memory Enabled
+    Your conversations are saved and can be resumed anytime.
+
+    Type your message to begin...
+    Type 'exit/quit' to stop the conversation
+    """
+
+    return banner
+
+print(show_banner())
+username = input("LeadX AI , enter your username ->  ")
+print(f"\n Restoring chat history for '{username}'...\n")
+
+while True:
+    user_mssg = input("you -> ")
+    if user_mssg.lower() == "quit" or user_mssg.lower() == "exit":
+        break
+    response = bot.invoke({"messages":[HumanMessage(content=user_mssg)]} , config={"configurable":{"thread_id":username}})
+    print("leadx -> " , response["messages"][-1].content)
+
+    
+    
